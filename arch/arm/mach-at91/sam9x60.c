217237e9f3ca8dcdaaee90cd600aac0e517e0669
Roger Amstell <rogera@rapsystems.co.uk>
2021-09-17 16:28:01 +0100

    Flash timings added

--- arch/arm/mach-at91/sam9x60.c
+++ arch/arm/mach-at91/sam9x60.c
@@ -10,16 +10,41 @@
 #include <linux/of.h>
 #include <linux/of_platform.h>
 
+#include <asm/io.h>
 #include <asm/mach/arch.h>
 #include <asm/system_misc.h>
 
 #include "generic.h"
+
+#include "at91sam9_smc.h" 
+
+static void flash_setup( void )
+{
+	struct	at91_smc * smc = (struct at91_smc * )ATMEL_BASE_SMC;
+
+	/* Configure SMC CS3 for NAND/SmartMedia */
+	writel(AT91_SMC_SETUP_NWE(10), &smc->cs[3].setup);		// 4
+
+	writel(AT91_SMC_PULSE_NWE(15) | AT91_SMC_PULSE_NCS_WR(25) |			// 10 20
+	       AT91_SMC_PULSE_NRD(15) | AT91_SMC_PULSE_NCS_RD(25),
+	       &smc->cs[3].pulse);
+
+	writel(AT91_SMC_CYCLE_NWE(30) | AT91_SMC_CYCLE_NRD(30),		// 20 20
+	       &smc->cs[3].cycle);
+
+	writel(AT91_SMC_MODE_RM_NRD | AT91_SMC_MODE_WM_NWE |
+	       AT91_SMC_MODE_DBW_8 |
+	       AT91_SMC_MODE_TDF | AT91_SMC_MODE_TDF_CYCLE(20),		// 15
+	       &smc->cs[3].mode);
+}
 
 static void __init sam9x60_init(void)
 {
 	of_platform_default_populate(NULL, NULL, NULL);
 
 	sam9x60_pm_init();
+
+	flash_setup( );
 }
 
 static const char *const sam9x60_dt_board_compat[] __initconst = {
